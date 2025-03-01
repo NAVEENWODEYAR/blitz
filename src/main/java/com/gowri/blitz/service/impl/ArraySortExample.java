675a577b40aa400af6fa7fe459a45020b76b6213
Naveen <naveenkrishnawodeyar@gmail.com>
2025-03-01 20:23:09 +0530

    sorting

---
+++ src/main/java/com/gowri/blitz/service/impl/ArraySortExample.java
@@ -0,0 +1,19 @@
+package com.gowri.blitz.service.impl;/*
+ * @author NaveenWodeyar
+ * @date 01-03-2025
+ */
+
+import java.util.Arrays;
+
+public class ArraySortExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 5, 6};
+
+        // Sorting the array
+        Arrays.sort(numbers);
+
+        // Printing the sorted array
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+}
+
