9f0810ea00f87e1899586535b3cc49fd50db77c8
Naveen <naveenkrishnawodeyar@gmail.com>
2025-03-23 20:20:08 +0530

    unit test cases added for consumer&producer methods

--- src/main/java/com/gowri/blitz/consumers/KafkaConsumerService.java
+++ src/main/java/com/gowri/blitz/consumers/KafkaConsumerService.java
@@ -2,8 +2,10 @@ package com.gowri.blitz.consumers;
 
 import com.google.gson.Gson;
 import com.gowri.blitz.modal.Student;
+import com.gowri.blitz.service.impl.StudentImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,9 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumerService {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumerService.class);
 
+    @Autowired
+    private StudentImpl studentImpl;
+
     @Value("${kafka.consumer.test}")
     String topic;
 
@@ -25,6 +30,11 @@ public class KafkaConsumerService {
         try {
             log.info("Received message: {}", message);
             Student student = new Gson().fromJson(message, Student.class);
+            try{
+                studentImpl.addStudent(student);
+            }catch (Exception e){
+                log.error("Exception occured while savig student:{}",e.getMessage());
+            }
             System.out.println("Message consumed successfully: " + message);
             log.debug("Student :{}",student);
         } catch (Exception e) {
